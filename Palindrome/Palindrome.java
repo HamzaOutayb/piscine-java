135db0339c8796d8d459fdf8b5845200c6135503
houtaib <houtayb@gmail.com>
2025-08-11 15:18:30 +0100

    palindrom

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,10 +1,10 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
-        for (int i = s.length() - 1, j = 0; i >= 0 && j < s.length(); i--, j++) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-        }
-        return true;
+        String rev = "";
+        for (int i = s.length()-1; i >= 0; i--) {
+            rev += s.charAt(i);
+       }
+
+        return rev.toLowerCase().equals(s.toLowerCase());
     }
 }
