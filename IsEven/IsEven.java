998f2cdec875837aefba1f57ce253b31e6f8e243
houtaib <houtayb@gmail.com>
2025-08-11 11:22:36 +0100

    iseven

--- IsEven/IsEven.java
+++ IsEven/IsEven.java
@@ -2,10 +2,6 @@ package IsEven;
 
 public class IsEven {
     public static boolean isEven(int a) {
-        if (a%2 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return a%2 == 0;
     }
 }
