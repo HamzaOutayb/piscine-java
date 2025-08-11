6615b4493d2afe7dba7c14389386d86b5872c869
houtaib <houtayb@gmail.com>
2025-08-11 15:14:04 +0100

    first quest

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,9 +1,10 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
-        String rev = "";
-        for (int i = s.length()-1; i >= 0; i--) {
-            rev += s.charAt(i);
-       }
-        return rev.equals(s);
+        for (int i = s.length() - 1, j = 0; i >= 0 && j < s.length(); i--, j++) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
