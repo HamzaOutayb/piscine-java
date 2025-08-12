d9e5575f2b621a74d88576835190657ad86608f5
houtaib <houtayb@gmail.com>
2025-08-12 15:11:02 +0100

    File Manager

--- FileSearch/FileSearch.java
+++ FileSearch/FileSearch.java
@@ -2,11 +2,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Optional;
-import java.util.stream.Stream;
+
 
 public class FileSearch {
-    public static String searchFile(String fileName) {
+    public static String searchFile(String fileName) throws IOException {
         return Files.walk(Paths.get("documents"))
                 .filter((path) -> path.getFileName().toString().equals(fileName))
                 .findFirst()
