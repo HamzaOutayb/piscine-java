eac7f6c5be87652bcf16e39665f85ce175d39e08
houtaib <houtayb@gmail.com>
2025-08-12 15:10:16 +0100

    File Manager

--- FileSearch/FileSearch.java
+++ FileSearch/FileSearch.java
@@ -7,10 +7,10 @@ import java.util.stream.Stream;
 
 public class FileSearch {
     public static String searchFile(String fileName) {
-        return Files.walk(Path.of("documents"))
-        .filter((path)-> path.getFileName().toString().equals(fileName))
-        .findFirst()
-        .map(Path::toString)
-        .orElse(null);
+        return Files.walk(Paths.get("documents"))
+                .filter((path) -> path.getFileName().toString().equals(fileName))
+                .findFirst()
+                .map(Path::toString)
+                .orElse(null);
     }
 }
