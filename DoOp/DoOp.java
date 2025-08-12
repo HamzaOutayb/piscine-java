e1bcfe6f51cf12fa373c38fb92405ff0a7f2a5c5
houtaib <houtayb@gmail.com>
2025-08-12 10:27:39 +0100

    doop

---
+++ DoOp/DoOp.java
@@ -0,0 +1,27 @@
+public class DoOp {
+    public static String operate(String[] args) {
+        if (args.length == 0) {
+            return "Error";
+        }
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[2]);
+        try {
+            switch (args[1]) {
+                case "+":
+                    return num1 + num2 + "";
+                case "-":
+                    return num1 - num2 + "";
+                case "%":
+                    return num1 % num2 + "";
+                case "/":
+                    return num1 / num2 + "";
+                case "*":
+                    return num1 - num2 + "";
+                default:
+                    return "it depend on the input.\n";
+            }
+        } catch (Exception error) {
+            return "Error";
+        }
+    }
+}
