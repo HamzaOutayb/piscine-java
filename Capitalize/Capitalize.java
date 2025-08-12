9d94ac64d8bb6416ec5322ed4eead97d3565f28e
houtaib <houtayb@gmail.com>
2025-08-12 13:54:37 +0100

    cap

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -11,8 +11,8 @@ public class Capitalize {
             byte[] contentbytes = Files.readAllBytes(Path.of(args[0]));
             String content = new String(contentbytes);
 
-            String res = content.substring(0, 1).toUpperCase()+content.substring(1).toLowerCase();
-            Files.write(Path.of(args[1]), res.trim().getBytes());
+            String res = content.trim().substring(0, 1).toUpperCase()+content.trim().substring(1).toLowerCase();
+            Files.write(Path.of(args[1]), res.getBytes());
         } catch(IOException error) {
             throw error;
         }
