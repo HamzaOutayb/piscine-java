4b0d9f83f0cf12c38a1ad5512f7aa7572dd0b181
houtaib <houtayb@gmail.com>
2025-08-13 18:28:23 +0100

    start mass

---
+++ StarMass/CelestialObject.java
@@ -0,0 +1,93 @@
+import java.util.Objects;
+
+public class CelestialObject {
+    private int mass;
+    private double x;
+    private double z;
+    private double y;
+    private String name;
+    public static final double KM_IN_ONE_AU = 1.5E8;
+
+    public CelestialObject() {
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z, int mass) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+        this.mass = mass;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static double getDistanceBetween(CelestialObject defaultStar, CelestialObject earth) {
+        double DeltaX = defaultStar.getX() - earth.getX();
+        double DeltaY = defaultStar.getY() - earth.getY();
+        double DeltaZ = defaultStar.getZ() - earth.getZ();
+        return Math.sqrt(DeltaX * DeltaX + DeltaY * DeltaY + DeltaZ * DeltaZ);
+    }
+
+    public static double getDistanceBetweenInKm(CelestialObject defaultStar, CelestialObject earth) {
+        double DeltaX = defaultStar.getX() - earth.getX();
+        double DeltaY = defaultStar.getY() - earth.getY();
+        double DeltaZ = defaultStar.getZ() - earth.getZ();
+        return Math.sqrt(DeltaX * DeltaX + DeltaY * DeltaY + DeltaZ * DeltaZ) * KM_IN_ONE_AU;
+    }
+
+    public String toString() {
+        return String.format("%s is positioned at (%.3f, %.3f, %.3f)", this.name, this.x, this.y, this.z);
+    }
+
+    public boolean equals(Object earth) {
+        CelestialObject earth1 = (CelestialObject)earth;
+        if (earth1 == null) {
+            return false;
+        }
+
+        return this.x == earth1.x && this.y == earth1.y && this.z == earth1.z && this.name.equals(earth1.name);
+    }
+
+    public int hashCode() {
+        return Objects.hash(this.name, this.x, this.y, this.z);
+    }
+
+    public int getMass() {
+        return this.mass;
+    }
+
+    public void setMass(int mass) {
+        this.mass = mass;
+    }
+}
---
+++ StarMass/Galaxy.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Galaxy {
+    private List<CelestialObject> celestialObjects;
+
+    public Galaxy() {
+        this.celestialObjects = new ArrayList<>();
+    }
+
+    public List<CelestialObject> getCelestialObjects() {
+        return celestialObjects;
+    }
+
+    public void addCelestialObject(CelestialObject ele) {
+        this.celestialObjects.add(ele);
+    }
+
+    public HashMap<String, Integer> computeMassRepartition() {
+        HashMap mymap = new HashMap<>();
+        mymap.put("Planet", 0);
+        mymap.put("Star", 0);
+        mymap.put("Other", 0);
+        for (CelestialObject num : celestialObjects) {
+            if (num instanceof Planet) {
+                Integer mass = (Integer) mymap.get("Planet");
+                mymap.put("Planet", mass + num.getMass());
+            } else if (num instanceof Star) {
+                Integer mass = (Integer) mymap.get("Star");
+                mymap.put("Star", mass + num.getMass());
+
+            } else {
+                Integer mass = (Integer) mymap.get("Other");
+                mymap.put("Other", mass + num.getMass());
+
+            }
+        }
+    }
+}
