599d5960853b34580e904f9911c2488f2844800c
houtaib <houtayb@gmail.com>
2025-08-13 14:34:05 +0100

    starstatic

--- StarStatic/CelestialObject.java
+++ StarStatic/CelestialObject.java
@@ -19,18 +19,42 @@ public class CelestialObject {
         this.z = z;
         this.name = name;
     }
+        public double getX() {
+        return this.x;
+    }
+    public double getY() {
+        return this.y;
+    }
+    public double getZ() {
+        return this.z;
+    }
+    public String getName() {
+        return this.name;
+    }
 
+    public void setX(double x) {
+         this.x = x;
+    }
+    public void setY(double y) {
+         this.y = y;
+    }
+    public void setZ(double z) {
+         this.z = z;
+    }
+    public void setName(String name) {
+         this.name = name;
+    }
     public static double getDistanceBetween(CelestialObject defaultStar, CelestialObject earth) {
-        double DeltaX = defaultStar.x - earth.x;
-        double DeltaY = defaultStar.y - earth.y;
-        double DeltaZ = defaultStar.z - earth.z;
+        double DeltaX = defaultStar.getX() - earth.getX();
+        double DeltaY = defaultStar.getY() - earth.getY();
+        double DeltaZ = defaultStar.getZ() - earth.getZ();
         return Math.sqrt(DeltaX * DeltaX + DeltaY * DeltaY + DeltaZ * DeltaZ);
     }
 
     public static double getDistanceBetweenInKm(CelestialObject defaultStar, CelestialObject earth) {
-        double DeltaX = defaultStar.x - earth.x;
-        double DeltaY = defaultStar.y - earth.y;
-        double DeltaZ = defaultStar.z - earth.z;
+       double DeltaX = defaultStar.getX() - earth.getX();
+        double DeltaY = defaultStar.getY() - earth.getY();
+        double DeltaZ = defaultStar.getZ() - earth.getZ();
         return Math.sqrt(DeltaX * DeltaX + DeltaY * DeltaY + DeltaZ * DeltaZ)*KM_IN_ONE_AU;
     }
 }
