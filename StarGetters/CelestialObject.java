25a9bc912f2dc2e4ba28b7e28063f82c620a9801
houtaib <houtayb@gmail.com>
2025-08-13 13:22:41 +0100

    sgetter

---
+++ StarGetters/CelestialObject.java
@@ -0,0 +1,46 @@
+public class CelestialObject {
+    private double x;
+    private double z;
+    private double y;
+    private String name;
+
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public Double getX() {
+        return this.x;
+    }
+    public Double getY() {
+        return this.y;
+    }
+    public Double getZ() {
+        return this.z;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setX(double x) {
+         this.x = x;
+    }
+    public void setY(double y) {
+         this.y = y;
+    }
+    public void setZ(double z) {
+         this.z = z;
+    }
+    public void setName(String name) {
+         this.name = name;
+    }
+}
