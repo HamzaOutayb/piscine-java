2dc7e9980ee02128480c7fa4f590adf2068d8fd8
houtaib <houtayb@gmail.com>
2025-08-18 18:17:45 +0100

    weapon

---
+++ AdventureWeapon/Monster.java
@@ -0,0 +1,29 @@
+public class Monster extends Character {
+    public Monster(String name, int maxHealth, Weapon weapon) {
+        super(name, maxHealth, weapon);
+    }
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() != 0) {
+            return String.format("%s is a monster with %d HP", this.getName(), this.getCurrentHealth());
+        }
+        return String.format("%s is a monster and is dead", this.getName());
+    }
+
+
+    @Override
+    public void takeDamage(int take) {
+        int reduced = (int) (take * 80/100);
+
+        if (reduced >= this.getCurrentHealth()) {
+            this.setCurrentHealth(0);
+        } else {
+            this.setCurrentHealth(this.getCurrentHealth()-reduced);
+        }
+    }
+
+    @Override
+    public void attack(Character other) {
+        other.takeDamage(7);
+    }
+}
---
+++ AdventureWeapon/Sorcerer.java
@@ -0,0 +1,48 @@
+public class Sorcerer extends Character implements Healer {
+    private final int healCapacity;
+
+    public Sorcerer(String name, int maxHealth, int healCapacity, Weapon weapon) {
+        super(name, maxHealth, weapon);
+        this.healCapacity = healCapacity;
+    }
+
+    @Override
+    public int getHealCapacity() {
+        return this.healCapacity;
+    }
+
+    @Override
+    public void heal(Character c) {
+        if (c.getCurrentHealth() + getHealCapacity() <= c.getMaxHealth()) {
+            c.setCurrentHealth(c.getCurrentHealth() + getHealCapacity());
+        } else {
+            c.setCurrentHealth(c.getMaxHealth());
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() == 0) {
+            return String.format("%s is a dead sorcerer. So bad, it could heal %d HP.", this.getName(),
+                    this.getHealCapacity());
+        }
+        return String.format("%s is a sorcerer with %d HP. It can heal %d HP.", this.getName(), this.getCurrentHealth(),
+                this.getHealCapacity());
+    }
+
+    @Override
+    public void takeDamage(int take) {
+        if (take > this.getCurrentHealth()) {
+            this.setCurrentHealth(0);
+            return;
+        }
+        this.setCurrentHealth(this.getCurrentHealth()-take);
+    }
+
+    @Override
+    public void attack(Character other) {
+        this.heal(this);
+        other.takeDamage(10);
+    }
+
+}
