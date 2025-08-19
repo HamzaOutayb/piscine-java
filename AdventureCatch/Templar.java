687af16aaf9a46111225d33bdeed84feef428c2f
houtaib <houtayb@gmail.com>
2025-08-19 12:42:16 +0100

    catch

--- AdventureCatch/Templar.java
+++ AdventureCatch/Templar.java
@@ -31,7 +31,7 @@ public class Templar extends Character implements Healer, Tank {
     @Override
     public String toString() {
         if (this.getCurrentHealth() == 0) {
-            return String.format("%s The templar Perceval is dead.",
+            return String.format("%s has been beaten, even with its %d shield. So bad, it could heal %d HP.",
                     this.getName(), this.getShield(), this.healCapacity)+String.format(" He has the weapon %s", this.getWeapon().toString());
         }
         return String.format("%s is a strong Templar with %d HP. It can heal %d HP and has a shield of %d.",
