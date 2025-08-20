7d8083a9e716335314d71a282efa6c002cd05d9d
houtaib <houtayb@gmail.com>
2025-08-20 11:26:21 +0100

    Sort list

---
+++ SortList/SortList.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortList {
+
+    public static List<Integer> sort(List<Integer> list) {
+        List<Integer> res = new ArrayList<>(list);
+        for (int i = 0; i < res.size(); i++) {
+            for (int j = i + 1; j < res.size(); j++) {
+                if (res.get(i) > res.get(j)) {
+                    Integer temp = res.get(i);
+                    res.set(i, res.get(j));
+                    res.set(j, temp);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static List<Integer> sortReverse(List<Integer> list) {
+        List<Integer> res = new ArrayList<>(list);
+        res.sort(Collections.reverseOrder());
+        return res;
+    }
+}
