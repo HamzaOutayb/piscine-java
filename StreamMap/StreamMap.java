d6702344b179a0f69313e931ecb16d9157107f47
houtaib <houtayb@gmail.com>
2025-08-20 12:46:17 +0100

    stream map

---
+++ StreamMap/StreamMap.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamMap {
+    public static Integer sumOfStringLength(Stream<String> s) {
+        return s.mapToInt(String::length).sum();
+    }
+
+    public static List<String> upperCaseAllString(Stream<String> s) {
+        return s.map(String::toUpperCase).collect(Collectors.toList());
+    }
+
+    public static Set<Integer> uniqIntValuesGreaterThan42(Stream<Double> s) {
+        return s.map(Double::intValue)
+                .filter(num -> num >= 42)
+                .collect(Collectors.toSet());
+    }
+}
