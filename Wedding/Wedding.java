7f03588e912364542d2c2be6cbe4823c4345b973
houtaib <houtayb@gmail.com>
2025-08-22 14:41:17 +0100

    wedding

---
+++ Wedding/Wedding.java
@@ -0,0 +1,18 @@
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Wedding {
+    public static Map<String, String> createCouple(Set<String> first, Set<String> second) {
+        Map<String, String> map = new HashMap<>();
+        List<String> list1 = new ArrayList<>(first);
+        List<String> list2 = new ArrayList<>(second);
+        int max = Math.max(second.size(), first.size());
+        for (int i = 0; i < second.size(); i++) {
+            map.put(list1.get(i), list2.get(i));
+        }
+        return map;
+    }
+}
