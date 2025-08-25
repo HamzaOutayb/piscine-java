e04d6fcdd89da4a3f898be88183fa56268f2cd20
houtaib <houtayb@gmail.com>
2025-08-25 17:40:57 +0100

    r

---
+++ RegexReplace/RegexReplace.java
@@ -0,0 +1,55 @@
+import java.util.regex.Pattern;
+
+public class RegexReplace {
+    public static String removeUnits(String s) {
+        if (s == null)
+            return null;
+        boolean itMatch = s.matches("(\\d+)cm.*(\\d+)€");
+        if (itMatch) {
+            s = s.replaceAll("cm", "").replaceAll("€", "");
+        }
+        return s;
+    }
+
+    public static String obfuscateEmail(String s) {
+        String[] slice = s.split("@");
+        String username = slice[0];
+        String[] otherpart = (slice[1]).split("\\.");
+        if (username.length() > 3) {
+            StringBuilder maskedUsername = new StringBuilder(username);
+
+            for (int i = 3; i < username.length(); i++) {
+                maskedUsername.setCharAt(i, '*');
+            }
+            username = maskedUsername.toString();
+        }
+
+        if (otherpart.length == 2) {
+            for (int i = 0; i < otherpart.length; i++) {
+                
+                StringBuilder domain = new StringBuilder(otherpart[i]);
+                     if (i == otherpart.length-1 && otherpart[i].equals("com") || otherpart[i].equals(".org") || otherpart[i].equals(".net")){
+                        continue;
+                    }
+                for (int j = 0; j < domain.length(); j++) {
+                    domain.setCharAt(j, '*');
+                }
+                otherpart[i] = domain.toString();
+            }
+        } else if (otherpart.length == 3) {
+            for (int i = 0; i < otherpart.length; i++) {
+                StringBuilder domain = new StringBuilder(otherpart[i]);
+                if (i != 1) {
+                    if (i == otherpart.length-1 && otherpart[i].equals("com") || otherpart[i].equals(".org") || otherpart[i].equals(".net")){
+                        continue;
+                    }
+                for (int j = 0; j < domain.length(); j++) {
+                        domain.setCharAt(j, '*');
+                    }
+                }
+                    otherpart[i] = domain.toString();
+            }
+        }
+        return username + "@" + String.join(".", otherpart);
+    }
+}
